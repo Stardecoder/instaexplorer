4d46d30671893785aa4e98b7a8441526433ff5a3
Stardecoder <nedjmeddine.allab@gmail.com>
2022-09-26 20:35:19 +0200

    init

---
+++ __main__.py
@@ -0,0 +1,22 @@
+import instaloader
+
+username = "dean_waves"
+password = "Nedjmeddine1988!"
+
+
+L = instaloader.Instaloader()
+L.login(username, password)  # (login)
+
+
+profile = instaloader.Profile.from_username(L.context, username)
+
+# Print list of followees
+follow_list = [f for f in profile.get_followers()]
+
+f = follow_list[4]
+
+followers_of_f = [f.get_followers()]
+for ff in followers_of_f:
+    print(ff)
+
+
